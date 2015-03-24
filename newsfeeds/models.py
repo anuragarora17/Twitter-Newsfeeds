6a3180a22fab19aec4940730c3a27d127f7208b7
Raghvendra Tripathi <tripathiraghvendra604@gmail.com>
2015-03-24 20:49:30 +0530

    Models made * name of database = newsfeeds

--- newsfeeds/models.py
+++ newsfeeds/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.IntegerField(primary_key=True)
+
+    def __str__(self):
+        return self.name
+
+
+class News(models.Model):
+    heading = models.CharField(max_length=100)
+    body = models.TextField()
+    time = models.DateTimeField(auto_now_add=True)
+    link = models.CharField(max_length=200)
+    country = models.ForeignKey(Country)
+
+    def __str__(self):
+        return self.heading
