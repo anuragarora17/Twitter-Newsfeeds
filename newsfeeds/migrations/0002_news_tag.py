2a6be1cff48798cadded812754b9d308314bcaf0
Animesh Kumar <animesh0721@gmail.com>
2015-04-14 17:41:18 +0530

    Adds tag field to News model
    * adds tag field which stores the original hash tag

---
+++ newsfeeds/migrations/0002_news_tag.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('newsfeeds', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='news',
+            name='tag',
+            field=models.CharField(default='#tag', max_length=100),
+            preserve_default=False,
+        ),
+    ]
